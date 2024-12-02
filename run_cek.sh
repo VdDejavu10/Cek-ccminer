1f52034ca5b6500e7d0d32873180e841520cd075
Vd Dejavu <ade.s.putra87@gmail.com>
2024-12-02 14:09:59 +0700

    Create run_cek.sh

---
+++ run_cek.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SERVICE="ccminer"
+DATE=$(date '+%d/%m/%Y %H:%M:%S')
+if pgrep -x "$SERVICE" >/dev/null
+then
+    echo "$DATE $SERVICE is running" >> run_cek.log
+else
+    echo "$DATE $SERVICE stopped" >> run_cek.log
+    bash /root/ccminer/run > /root/ccminer/miner.log 2>&1
+    # uncomment to start nginx if stopped
+    # systemctl start nginx
+    # mail  
+fi
