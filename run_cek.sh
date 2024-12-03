c9af85395a3e983fa84e3d33b1a232e984db4fc4
Vd Dejavu <ade.s.putra87@gmail.com>
2024-12-03 16:06:54 +0700

    Update run_cek.sh

--- run_cek.sh
+++ run_cek.sh
@@ -6,6 +6,7 @@ then
     echo "$DATE $SERVICE is running" >> run_cek.log
 else
     echo "$DATE $SERVICE stopped" >> run_cek.log
+     echo "$DATE $SERVICE executed" >> run_cek.log
     bash /root/ccminer/run > /root/ccminer/miner.log 2>&1
     echo "$DATE $SERVICE started" >> run_cek.log
     bash /root/run_cek.sh
